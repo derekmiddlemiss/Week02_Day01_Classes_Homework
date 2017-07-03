7b26c25d0dbaaac02e2a6b57cd37d1de7b9b5078
Derek Middlemiss <derekmiddlemiss@gmail.com>
2017-07-03 19:24:08 +0100

    Optional exercises complete

--- optional.rb
+++ optional.rb
@@ -8,4 +8,37 @@ class Library
     return @books
   end
 
+  def detail_single_book(book_title)
+    for book in @books
+      return book if ( book[:title] == book_title )
+    end
+#    return @books.select { |book| book[:title] == book_title }
+  end
+
+  def rental_detail_single_book(book_title)
+    for book in @books
+      return book[:rental_details] if ( book[:title] == book_title )
+    end
+  end 
+
+  def add_book(new_title)
+    new_book = {
+      title: new_title,
+      rental_details: {
+        student_name: "",
+        date: ""
+      }
+    }
+    @books.push(new_book)
+  end
+
+  def change_rental_details(title, new_student, new_date)
+    for book in @books
+      if book[:title] = title
+        book[:rental_details][:student_name] = new_student
+        book[:rental_details][:date] = new_date
+      end
+    end
+  end
+  
 end
--- optional_spec.rb
+++ optional_spec.rb
@@ -1,33 +1,71 @@
 require('minitest/autorun')
-require_relative('./homework')
+require_relative('./optional')
 
 class TestHomework < MiniTest::Test
 
-  def test_initialize_Library
-    miskatonic_library = Library.new( [
+  def setup
+    @miskatonic = Library.new( [
       {
         title: "The Necronomicon",
         rental_details: {
           student_name: "Howard P. Lovecraft",
           date: "01/12/1926"
         }
-        },
-        {
-          title: "Unauchsprechlichen Kulten",
-          rental_details: {
-            student_name: "August Derleth",
-            date: "06/06/1927"
-          }
-          },
-          {
-            title: "De Vermis Mysteriis",
-            rental_details: {
-              student_name: "Robert Bloch",
-              date: "01/02/1926"
-            }
-          }
-          ] )
-    print miskatonic_library.list_books().class
+      },
+      {
+        title: "Unauchsprechlichen Kulten",
+        rental_details: {
+          student_name: "August Derleth",
+          date: "06/06/1927"
+        }
+      },
+      {
+        title: "De Vermis Mysteriis",
+        rental_details: {
+          student_name: "Robert Bloch",
+          date: "01/02/1926"
+        }
+      }
+    ] )
+  end  
+
+  def test_initialize_library
+    assert_equal("The Necronomicon", @miskatonic.list_books[0][:title])
   end
+
+  def test_detail_single_book
+    title = "The Necronomicon"
+    output = @miskatonic.detail_single_book(title)
+    assert_equal("Howard P. Lovecraft",output[:rental_details][:student_name])
+  end
+
+  def test_rental_detail_single_book
+    title = "De Vermis Mysteriis"
+    output = @miskatonic.rental_detail_single_book(title)
+    expected = {
+          student_name: "Robert Bloch",
+          date: "01/02/1926"
+    }
+    assert_equal(expected, output)
+  end
+
+  def test_add_book
+    new_title = "The King in Yellow"
+    @miskatonic.add_book(new_title)
+    assert_equal("The King in Yellow", @miskatonic.list_books[3][:title])
+  end
+
+  def test_change_rental_details
+    title = "Unauchsprechlichen Kulten"
+    new_student = "Brian Lumley"
+    new_date = "10/02/1988"
+    @miskatonic.change_rental_details(title, new_student, new_date)
+    output = @miskatonic.rental_detail_single_book("Unauchsprechlichen Kulten")
+    expected = {
+      student_name: "Brian Lumley",
+      date: "10/02/1988"
+    }
+    assert_equal(expected, output)
+  end 
 
 end     
